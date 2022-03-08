b703e25b1b0b438043618e33d8a1e64cac2b1e00
Adam Andersson <adander@kth.se>
2022-03-08 18:20:03 +0800

    Basic indexing on all documents

--- HW3/index.py
+++ HW3/index.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
+import math
+import os
+import bisect
+import pickle
 import re
 import nltk
 import sys
 import getopt
 
-def usage():
-    print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
+PORTER_STEMMER = nltk.stem.porter.PorterStemmer()
+STOP_WORDS = set(nltk.corpus.stopwords.words('english') + [".", ",", ";", ":"])
+NUMBER_OF_BLOCKS = 10
+
+
+def normalize_token(token):
+    """
+    Case-folds and porter-stems a token (str word). Returns a normalized token (str word).
+    """
+    token = token.lower()  # case folding
+    token = PORTER_STEMMER.stem(token)  # porter-stemming
+    return token
+
 
 def build_index(in_dir, out_dict, out_postings):
     """
@@ -13,8 +28,114 @@ def build_index(in_dir, out_dict, out_postings):
     then output the dictionary file and postings file
     """
     print('indexing...')
-    # This is an empty method
-    # Pls implement your code in below
+
+    # Wipe all contents from the files before running the code. Because we are appending (open with 'ab')
+    # dictionaries from every block, we want to start from clean files.
+    open(out_dict, 'w').close()
+    open(out_postings, 'w').close()
+
+    term_to_term_id = {}  # term (str) -> term id (int, 4 bytes)
+    term_id_to_term = {}  # term id (int, 4 bytes) -> term (str)
+    term_id = 1  # we keep a global term id that we will assign to tokens when processing them
+
+    all_documents = [int(f) for f in os.listdir(in_dir)]
+    all_documents.sort()    # TODO: Evaluate if it is OK to sort this. It makes the later implementations easier.
+
+    dictionary = {}
+    postings_list = {}
+
+    for doc_id in all_documents:
+        with open(os.path.join(in_dir, str(doc_id)), 'r') as doc_open:
+            doc_text = doc_open.read()
+
+        sentences = nltk.sent_tokenize(doc_text)
+
+        processed_document = []
+        for s in sentences:
+            words = nltk.word_tokenize(s)
+
+            # case-fold all word tokens, then porter-stem the word
+            processed_document.append([normalize_token(token) for token in words])
+
+        for sentence in processed_document:
+            for token in sentence:
+                if token not in term_to_term_id:
+                    # if it is the first time we see this term, we add it to our dictionaries of terms and term ids
+                    term_to_term_id[token] = term_id
+                    term_id_to_term[term_id] = token
+                    term_id += 1
+
+                tokens_term_id = term_to_term_id[token]
+
+                if tokens_term_id not in dictionary:
+                    # first time seeing it, so it has only been seen in the current document (i.e. doc freq (block) = 1)
+                    dictionary[tokens_term_id] = 1
+                    postings_list[tokens_term_id] = [(doc_id, 1)]   # initialise term freq to 1 (2nd term)
+                    # every posting in a postings list is a tuple (doc_id, term_freq)
+                else:
+                    # first time seeing this term for this doc
+                    if doc_id != postings_list[tokens_term_id][-1][0]:
+                        dictionary[tokens_term_id] += 1  # only increment for first occurrence in each docu
+
+                        " This was used when not sorting the documents before starting to process them "
+                        # bisect is a built-in module. Uses binary search [O(log n)] to
+                        # insert element into a sorted list.
+                        # bisect.insort(postings_list[tokens_term_id], (doc_id, 1))
+
+                        postings_list[tokens_term_id].append((doc_id, 1))
+
+                    else:
+                        # if we have already seen this token in this posting already,
+                        # then we should add to its term frequency.
+
+                        postings_list[tokens_term_id][-1] = (postings_list[tokens_term_id][-1][0], postings_list[tokens_term_id][-1][1] + 1)
+
+                        """
+                        for i in range(len(postings_list[tokens_term_id])):
+                            posting = postings_list[tokens_term_id][i]
+                            if posting[0] == doc_id:
+                                # can not modify a tuple, need to overwrite, hence:
+                                postings_list[tokens_term_id][i] = (posting[0], posting[1] + 1)
+                                break   # this can only happen once so we break when this happens
+                                
+                        """
+
+    print("... done with reading / writing blocks")
+
+    with open('term_conversion.txt', 'wb') as term_conversion:
+        pickle.dump(term_to_term_id, term_conversion)
+        pickle.dump(term_id_to_term, term_conversion)
+
+    with open(out_postings, 'wb') as write_postings:
+        for term_id, posting_list in postings_list.items():
+            writer_position = write_postings.tell()
+            pickle.dump(posting_list, write_postings)
+
+            # every term_id in the dictionary will be a tuple of (doc_frequency, writer offset)
+            dictionary[term_id] = (dictionary[term_id], writer_position)
+
+    with open(out_dict, 'wb') as write_dict:
+        pickle.dump(dictionary, write_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### END OF FILE. RANDOM STUFF BELOW ###
+
+
+def usage():
+    print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
+
 
 input_directory = output_file_dictionary = output_file_postings = None
 
--- HW3/search.py
+++ HW3/search.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
+import pickle
 import re
 import nltk
 import sys
 import getopt
 
-def usage():
-    print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
+
+def retrieve_postings_list(dictionary, term_id):
+    """
+    Takes a term id and retrieves its posting list by using the dictionary to find the offset
+    in the file the posting list was written to. Returns said postings list.
+    """
+    with open(postings_file, 'rb') as read_postings:
+        reader_offset = dictionary[term_id][1]
+        read_postings.seek(reader_offset)
+        return pickle.load(read_postings)
+
 
 def run_search(dict_file, postings_file, queries_file, results_file):
     """
@@ -13,8 +23,38 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     perform searching on the given queries file and output the results to a file
     """
     print('running search on the queries...')
-    # This is an empty method
-    # Pls implement your code in below
+
+    with open(dict_file, 'rb') as read_dict:
+        # We are able to read the full dictionary into memory
+        # The dictionary is structured as - term_id : (doc_freq, file_offset)
+        dictionary = pickle.load(read_dict)
+
+    with open('term_conversion.txt', 'rb') as read_term_converter:
+        term_to_term_id = pickle.load(read_term_converter)  # term (str) -> term id (int, 4 bytes)
+        term_id_to_term = pickle.load(read_term_converter)  # term id (int, 4 bytes) -> term (str)
+
+    test_id = term_to_term_id['the']
+    print(term_id_to_term[test_id])
+    print(retrieve_postings_list(dictionary, test_id))
+
+
+
+
+
+
+
+
+
+
+
+
+
+### END OF FILE. RANDOM STUFF BELOW ###
+
+
+def usage():
+    print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
+
 
 dictionary_file = postings_file = file_of_queries = output_file_of_results = None
 
